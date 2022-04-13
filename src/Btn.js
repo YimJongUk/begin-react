8aaf06c3fd0fdef5db73516ee123221a5b3b7b61
yju10623 <yju10623@jei.com>
2022-04-13 14:48:38 +0900

    처음시작

---
+++ src/Btn.js
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+function Btn({ text }) {
+  return (
+    <button className={styles.title}>
+      {text}
+    </button>
+  );
+}
+
+Btn.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Btn;
